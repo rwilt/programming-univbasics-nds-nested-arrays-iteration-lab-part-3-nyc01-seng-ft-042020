64b672aa91e4bb1641aaf01c28f8a113d3040c97
rosie w <rosie.wilt@gmail.com>
2020-04-10 15:42:57 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ row_index = 0
 while row_index < src.length do
   element_index = 0
   while element_index < src[row_index].length do
-    if src[row_index][element_index] == "string"
+    if src[row_index][element_index].class == String
     total += src[row_index][element_index]
   end
   element_index +=1
